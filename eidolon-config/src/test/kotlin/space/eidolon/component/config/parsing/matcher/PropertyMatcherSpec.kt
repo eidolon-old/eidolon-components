ecd7078beadbf921388b0960a2286a113a1469bf
Elliot Wright <wright.elliot@gmail.com>
2015-08-28 15:04:28 +0100

    Quiet down git.

--- eidolon-config/src/test/kotlin/space/eidolon/component/config/parsing/matcher/PropertyMatcherSpec.kt
+++ eidolon-config/src/test/kotlin/space/eidolon/component/config/parsing/matcher/PropertyMatcherSpec.kt
@@ -11,10 +11,48 @@
 
 package space.eidolon.component.config.parsing.matcher
 
+import org.jetbrains.spek.api.*
+import org.junit.*
+import space.eidolon.component.config.parsing.PropertyToken
+import space.eidolon.component.config.parsing.exception.MatchNotFoundException
+import kotlin.test.*
+
 /**
  * PropertyMatcherSpec
  *
  * @author Elliot Wright <elliot@elliotwright.co>
  */
-public class PropertyMatcherSpec {
-}
+class PropertyMatcherSpec : Spek() { init {
+    given("a property matcher") {
+        val matcher = PropertyMatcher()
+
+        on("instantiation") {
+            it("should be a PropertyMatcher") {
+                shouldBeTrue(matcher is PropertyMatcher)
+            }
+
+            it("should implement the Matcher interface") {
+                shouldBeTrue(matcher is Matcher)
+            }
+        }
+
+        on("matching input with a property at the start") {
+            val input = "property: 'value'"
+            val result = matcher.match(input)
+
+            it("should return a MatcherResult") {
+                shouldBeTrue(result is MatcherResult)
+            }
+        }
+
+        on("matching input with no property at the start") {
+            val input = "[ 'array' ]"
+
+            it("should throw a MatchNotFoundException") {
+                shouldThrow(javaClass<MatchNotFoundException>(), fun() {
+                    matcher.match(input)
+                })
+            }
+        }
+    }
+}}
