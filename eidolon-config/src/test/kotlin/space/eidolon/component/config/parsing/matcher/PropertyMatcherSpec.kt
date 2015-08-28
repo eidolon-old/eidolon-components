9c499e1549ba1cef4d02b18d12dbf96ef8acbb98
Elliot Wright <wright.elliot@gmail.com>
2015-08-28 15:03:56 +0100

    Case:SPARK-403: Updated icon font, updating layout to match actual design.

---
+++ eidolon-config/src/test/kotlin/space/eidolon/component/config/parsing/matcher/PropertyMatcherSpec.kt
@@ -0,0 +1,20 @@
+/**
+ * This file is part of the "eidolon-components" project.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the LICENSE is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package space.eidolon.component.config.parsing.matcher
+
+/**
+ * PropertyMatcherSpec
+ *
+ * @author Elliot Wright <elliot@elliotwright.co>
+ */
+public class PropertyMatcherSpec {
+}
